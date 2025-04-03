c1c805f4ae6ef1c65df9fa48690e34c0cbaecfec
00j5y <spmt.jay@gmail.com>
2025-04-03 23:22:36 +0200

    Fix login

--- main.py
+++ main.py
@@ -11,19 +11,20 @@ def login_insta(username,password):
     profile = instaloader.Instaloader()
     profile.login(username,password)
 
-def login():
+def log():
     username = input(f"\n[{Fore.GREEN}?{Fore.WHITE}] Username : ")
     password = input(f"[{Fore.GREEN}?{Fore.WHITE}] Password : ")
 
     try:
-        login(username, password)
+        login_insta(username, password)
     except:
         print(f"\n[{Fore.RED}!{Fore.WHITE}] Une erreur s'est produite, veuillez essayer a nouveau")
-        login()
+        log()
 
 def main():
     clear()
     print(f"[{Fore.GREEN}!{Fore.WHITE}] Instagram Followback Checker / cred: @00j5Y\n")
-    login()
+    log()
+    print(f"\n[{Fore.GREEN}!{Fore.WHITE}] Sucessfully connected\n")
 
 main()
